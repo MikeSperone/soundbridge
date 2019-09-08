abaa5f345344fda98244ac1c182a2d7e850df293
Michael Sperone <mike@mikesperone.com>
2019-09-08 11:51:29 -0500

    ambient - typescript

--- src/js/synths/ambient.ts
+++ src/js/synths/ambient.ts
@@ -2,11 +2,11 @@
 /**
  * Created by Mike on 8/25/16.
  */
-import Play from './play.js';
+import Play from './play';
 
 export default class Ambient extends Play {
 
-    constructor(audio, context) {
+    constructor(audio: string, context: AudioContext) {
         super(audio, context);
         this.play();
     }
