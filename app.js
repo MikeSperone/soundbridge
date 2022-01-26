139cf50fde018962c721987a003bf491e158aae4
Michael Sperone <mike@mikesperone.com>
2022-01-26 17:22:16 -0600

    moved local audio files

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var app = express();
 var path = require('path');
 
 var build_path = path.join(__dirname, '../public');
+var shared_path = path.join(__dirname, '../shared');
 
 app.use(express.static(build_path));
-app.use("audio", express.static(path.join(build_path, "audio")));
+app.use('/audio', express.static(path.join(shared_path, 'audio')));
 
-app.get("/test", function(req, res) {
+app.get('/test', function(req, res) {
     res.status(200)
         .sendFile(path.join(build_path, 'test.html'));
 });
